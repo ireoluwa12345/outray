e028a344c66229e20fb37d08f7b71710bfc3c406
akinloluwami <akinkunmioye42@gmail.com>
2025-12-12 21:46:10 +0100

    Update page title to include description of OutRay as an alternative to ngrok

--- web/src/routes/__root.tsx
+++ web/src/routes/__root.tsx
@@ -19,7 +19,7 @@ export const Route = createRootRoute({
         content: "width=device-width, initial-scale=1",
       },
       {
-        title: "OutRay",
+        title: "OutRay - an open source alternative to ngrok",
       },
     ],
   }),
